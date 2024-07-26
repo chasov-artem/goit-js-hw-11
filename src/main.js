43d7b98b6b4efac89b92967f3c3e791610b034ff
chasov-artem <chasov90@gmail.com>
2024-07-26 21:44:58 +0300

    styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 function searchImageByQuery(query) {
   const URL = 'https://pixabay.com/api/';
